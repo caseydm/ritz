f870df60006588ca9706d648fb78d24b4bbc3f0b
Casey M <caseym@gmail.com>
2016-09-01 21:58:04 -0400

    test updates

--- test_app.py
+++ test_app.py
@@ -1,4 +1,9 @@
-def test_dates():
-    assert 3 == 3
+from datetime import datetime
+from app import build_dates
+
+def test_dates(monkeypatch):
+    monkeypatch.setattr(datetime.now(), 'expanduser', mockreturn)
+    dates = build_dates()
+    assert datetime.now() == FAKETIME
 
 # today = datetime(2016, 6, 30)
